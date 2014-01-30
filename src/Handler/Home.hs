6111b8e31e65bcbf7bc57cf8a9846e8486cc8877
Mladen Srdić <m.s.qubits@gmail.com>
2014-01-30 10:00:04 +0000

    Dummy.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,6 +6,4 @@ getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
     setTitle "HKaido home page"
     getYesod >>= addScriptEither . urlJqueryJs
-    addStylesheetRemote "bootstrap/css/bootstrap-theme.min.css"
-    addStylesheetRemote "bootstrap/css/bootstrap.min.css"
     $(widgetFile "home")

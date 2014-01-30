7326bbf017ccf53e1ad3d2aa0f83c20449271054
Mladen Srdić <m.s.qubits@gmail.com>
2014-01-30 10:43:41 +0000

    I have no idea what I'm doing.

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,9 +1,12 @@
 module Handler.Home where
 
 import Import
+import Yesod.Static
 
-getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
+staticFiles "static"
+
+handleHomeR :: Handler Html
+handleHomeR = defaultLayout $ do
     setTitle "HKaido home page"
     getYesod >>= addScriptEither . urlJqueryJs
     addStylesheetRemote "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
